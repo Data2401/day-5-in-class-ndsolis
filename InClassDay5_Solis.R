327e457d04812d36fedab833214f53421293263f
Nicholas Solis <snickreal886@gmail.com>
2025-02-04 12:57:32 -0600

    day 5 commits

---
+++ InClassDay5_Solis.R
@@ -0,0 +1,54 @@
+# In Class "Lab" - Intro to R and Functions
+# Data 2041
+
+
+# This is an R script. 
+# Sometimes you don't need a full report, so you don't need a .Rmd
+# Put your answers in this .R file, name it InClassDay5_Yourlastname.R and
+# save/commit/push as usual
+# turn the repository in on Blackboard when you're done. 
+# The code should be able to run on my machine with no errors. (I will check!)
+
+my_name <- "Nicholas" 
+
+num_pets <- 3
+
+favorite_book <- "1984"
+
+radius <- 7
+
+area <- pi*radius^2
+
+too_big = area > 100 = TRUE
+  
+my_name
+area
+too_big
+
+name_length = 8
+print(name_length)
+now_doing <- "nicholas is programming!"
+
+paste(now_doing)
+
+
+# Make the `now_doing` variable upper case
+
+# Pick two of your favorite numbers (between 1 and 100) and assign them to 
+# variables `fav_1` and `fav_2`
+
+# Divide each number by the square root of 201 and save the new value in the
+# original variable
+
+# Create a variable `raw_sum` that is the sum of the two variables. Use the 
+# `sum()` function for practice.
+
+# Create a variable `round_sum` that is the `raw_sum` rounded to 2 decimal places.
+# Use the `round()` function.
+
+# Create two new variables `round_1` and `round_2` that are your `fav_1` and 
+# `fav_2` variables rounded to 2 decimal places
+
+# Create a variable `sum_round` that is the sum of the rounded values
+
+# Print out both variables and compare. Are they the same?
